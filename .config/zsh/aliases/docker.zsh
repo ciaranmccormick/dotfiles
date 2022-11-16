2940013793a5d2c479302549ea0aa9f9ae3d4301
Ciaran McCormick <ciaran@ciaranmccormick.com>
2022-11-16 00:56:22 +0000

    Add aliases for docker

---
+++ .config/zsh/aliases/docker.zsh
@@ -0,0 +1,10 @@
+alias dc='docker-compose'
+alias dcu='docker-compose up'
+alias dcud='docker-compose up -d'
+alias dcps='docker-compose ps'
+alias dcl='docker-compose logs'
+alias dclf='docker-compose logs -f'
+alias dcr='docker-compose run --rm'
+
+alias dps='docker ps'
+alias dv='docker volume'
