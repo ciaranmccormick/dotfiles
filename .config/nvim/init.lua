0613c3e93d86aae9f4c516d78d570235c320bdc2
Ciaran McCormick <ciaran@ciaranmccormick.com>
2023-01-19 19:28:14 +0000

    Highlight on yank

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -28,3 +28,12 @@ local opt = vim.opt
 opt.foldmethod = "expr"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.g.python3_host_prog = 0
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight_yank", {}),
+  desc = "Hightlight selection on yank",
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
+  end,
+})
