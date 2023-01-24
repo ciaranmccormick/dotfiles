61f367a0b50b5e8c2a560b2d7a1a51c53b331f35
Ciaran McCormick <ciaran@ciaranmccormick.com>
2023-01-24 18:33:20 +0000

    LazyVim configs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,41 +1,4 @@
--- User
-require "user.options"
-require "user.keymaps"
-require "user.colorscheme"
-require "user.plugins"
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
 
--- Systems
-require "user.cmp"
--- require "user.lsp"
-require "user.lsp-zero"
-require "user.null-ls"
-
--- Plugins
-require "user.impatient"
-require "user.lualine"
-require "user.telescope"
-require "user.treesitter"
-require "user.autopairs"
-require "user.gitsigns"
-require "user.comment"
-require "user.nvim-tree"
-require "user.toggleterm"
-require "user.whichkey"
-require "user.indentline"
-
-
-local vim = vim
-local opt = vim.opt
-
-opt.foldmethod = "expr"
-opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.g.python3_host_prog = 0
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-  group = vim.api.nvim_create_augroup("highlight_yank", {}),
-  desc = "Hightlight selection on yank",
-  pattern = "*",
-  callback = function()
-    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
-  end,
-})
+vim.g.loaded_python3_provider = 0
