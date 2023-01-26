8fa0e7e0cd5568046a82b5af863c1ac66d0803a4
Ciaran McCormick <ciaran@ciaranmccormick.com>
2023-01-26 23:11:45 +0000

    Updated nvim config

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -6,8 +6,17 @@ local keymap = vim.api.nvim_set_keymap
 
 keymap("", "<Space>", "<Nop>", opts)
 keymap("i", "jk", "<Esc>", opts)
-keymap("n", "<C-h>", "<C-w>h", opts         )
+
+keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("v", "p", '"_dP', opts)
+
+keymap("x", "p", "\"_dP", opts)
+
+-- Keep cursor in place for ctrl-d/u
+keymap("n", "<C-d>", "<C-d>zz", opts)
+keymap("n", "<C-u>", "<C-u>zz", opts)
+
+-- make current buffer executable
+keymap("n", "<leader>x", "<cmd>!chmod +x %<cr>", opts)
--- .config/nvim/lua/user/lsp/null-ls.lua
+++ .config/nvim/lua/user/lsp/null-ls.lua
@@ -1,29 +1,35 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  return
+	return
 end
 
 local f = null_ls.builtins.formatting
 local d = null_ls.builtins.diagnostics
 local ca = null_ls.builtins.code_actions
 local settings = {
-  debug = false,
-  sources = {
-    -- formatting
-    f.prettier,
-    f.black,
-    f.blue,
-    f.isort,
-    f.stylua,
-    f.rustfmt,
-    -- diagnostics
-    d.flake8,
-    d.eslint,
-    d.mypy,
-    d.codespell,
-    d.markdownlint,
-    -- code actions
-    ca.eslint,
-  },
+	debug = true,
+	timeout_ms = 5000,
+	sources = {
+		-- formatting
+    -- js/html/ts
+		f.prettier,
+    -- python
+		f.black,
+		f.isort,
+    -- lua
+		f.stylua,
+    -- rust
+		f.rustfmt,
+		-- diagnostics
+		d.eslint,
+    -- python
+		d.flake8,
+		d.mypy,
+    -- spelling/markdown
+		d.codespell,
+		d.markdownlint,
+		-- code actions
+		ca.eslint,
+	},
 }
 null_ls.setup(settings)
